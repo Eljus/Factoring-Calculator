4c36168da75f7bf0fd031b370ad4a9aa4163389e
MSI\eljus <eljusshipovski@gmail.com>
2022-11-06 14:47:30 +0200

    Factoring Calculator POJO class with all Query String Parameters

---
+++ api-tests/src/main/java/com/calculator/api/FactoringCalculate.java
@@ -0,0 +1,129 @@
+package com.calculator.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(fluent = true)
+public class FactoringCalculate {
+
+    @JsonProperty("v6")
+    private String v6;
+
+    @JsonProperty("v7")
+    private String v7;
+ 
+    @JsonProperty("v9")
+    private String v9;
+
+    @JsonProperty("ns")
+    private String ns;
+
+    @JsonProperty("pev2")
+    private String pev2;
+
+    @JsonProperty("l2")
+    private String l2;
+
+    @JsonProperty("mid")
+    private String mid;
+
+    @JsonProperty("v102")
+    private String v102;
+
+    @JsonProperty("v121")
+    private String v121;
+
+    @JsonProperty("pageName")
+    private String pageName;
+
+    @JsonProperty("v92")
+    private String v92;
+
+    @JsonProperty("aqb")
+    private String aqb;
+
+    @JsonProperty("v74")
+    private String v74;
+
+    @JsonProperty("v73")
+    private String v73;
+
+    @JsonProperty("v10")
+    private String v10;
+
+    @JsonProperty("v97")
+    private String v97;
+
+    @JsonProperty("v119")
+    private String v119;
+
+    @JsonProperty("aqe")
+    private String aqe;
+
+    @JsonProperty("v58")
+    private String v58;
+
+    @JsonProperty("v57")
+    private String v57;
+
+    @JsonProperty("v59")
+    private String v59;
+
+    @JsonProperty("events")
+    private String events;
+
+    @JsonProperty("mcorgid")
+    private String mcorgid;
+
+    @JsonProperty("ce")
+    private String ce;
+
+    @JsonProperty("ch")
+    private String ch;
+
+    @JsonProperty("g")
+    private String g;
+
+    @JsonProperty("lrt")
+    private String lrt;
+
+    @JsonProperty("ndh")
+    private String ndh;
+
+    @JsonProperty("t")
+    private String t;
+
+    @JsonProperty("pe")
+    private String pe;
+
+    @JsonProperty("pf")
+    private String pf;
+
+    @JsonProperty("v23")
+    private String v23;
+
+    @JsonProperty("v22")
+    private String v22;
+
+    @JsonProperty("v1")
+    private String v1;
+
+    @JsonProperty("v2")
+    private String v2;
+
+    @JsonProperty("v3")
+    private String v3;
+
+    @JsonProperty("v4")
+    private String v4;
+
+    @JsonProperty("ts")
+    private String ts;
+
+    @JsonProperty("v5")
+    private String v5;
+}
---
+++ api-tests/src/test/java/CalculatorTest.java
@@ -0,0 +1,66 @@
+import com.calculator.api.FactoringCalculate;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+    @BeforeClass
+    public void seuUp(){
+        RestAssured.baseURI = "https://swedbankab.d3.sc.omtrdc.net/b/ss/swedbankabbballswedprod/1/JS-2.9.0/s92561309652512";
+    }
+
+    @Test
+    public void abilityToCalculateFactoringCalculator(){
+        FactoringCalculate factoringCalculate = new FactoringCalculate()
+                .aqb("1")
+                .ndh("1")
+                .pf("1")
+                .t("6/10/2022 13:52:23 0 -120")
+                .ts("1667735544")
+                .mid("77711594932993099831277502136663481371")
+                .ce("UTF-8")
+                .ns("swedbankab")
+                .pageName("web/business/finance/trade/factoring")
+                .g("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG")
+                .ch("web/lt/business/finance")
+                .events("event146")
+                .v1("D=pageName")
+                .v2("D=ch")
+                .l2("v23;v73;v74;e146;pageName;ch;v1;v2;v3;v4;v5;v6;v7;v9;v10;v22;v57;v58;v59;v92;v97;v102;v119;v121")
+                .v2("lt-web-business")
+                .v4("223")
+                .v5("web")
+                .v6("eng")
+                .v7("0210")
+                .v9("business")
+                .v10("D=User-Agent")
+                .v22("desktop")
+                .v23("calculator-cta:calculate-factoring")
+                .v57("https://www.swedbank.lt/business/finance/trade/factoring")
+                .v58("D=r")
+                .v59("Faktoringas")
+                .v73("button")
+                .v74("calculator")
+                .v92("0")
+                .v97("lt")
+                .v102("language=ENG")
+                .v119("web/business/finance/trade/factoring")
+                .v121("Factoring")
+                .pe("lnk_o")
+                .pev2("click")
+                .mcorgid("AB12899B544ABE260A4C98BC")
+                .lrt("3")
+                .aqe("1");
+
+        RestAssured
+                .given().contentType(ContentType.JSON).log().all()
+                .body(factoringCalculate)
+                .when()
+                .get()
+                .then().log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+}
